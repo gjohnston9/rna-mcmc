6dff1e54bfb2e0f7ce9f956d183037b28765ed4d
Greg <greg9381@gmail.com>
2018-01-18 22:39:41 -0500

    formatting

--- mcmc.py
+++ mcmc.py
@@ -4,7 +4,7 @@
 # The markov chain will run t-steps
 # While the number of steps is still less than t:
     # From the current plane tree, pick a candidate new dyckword to move to
-    # Compute chanceOfMove=min (1, exp(-newWord)/exp(-currWord)) <---- this is MCMC
+    # Compute chanceOfMove=min (1, exp(-new_word)/exp(-curr_word)) <---- this is MCMC
     # with prob chanceOfMove, set newDyckWord to be our current dyckWord, otherwise keep current dyckWord as my state
 
 import matplotlib
@@ -23,23 +23,23 @@ import timeit
 from operator import add
 
 
-def isValid(dyckWord, end):
+def is_valid(dyck_word, end):
     """
-    this function checks for valid dyckWords/ballot sequences, ignoring letters past the `end` index
+    this function checks for valid dyck_words/ballot sequences, ignoring letters past the `end` index
     """
-    counter1=0
-    counter0=0
-    for letter in dyckWord[:end+1]:
-        if letter ==1:
-            counter1+=1
+    counter1 = 0
+    counter0 = 0
+    for letter in dyck_word[:end+1]:
+        if letter == 1:
+            counter1 += 1
         else:
-            counter0+=1
-        if counter0> counter1:
+            counter0 += 1
+        if counter0 > counter1:
             return False 
     return True
 
 
-def findPos(i, j, dyckWord):
+def find_pos(i, j, dyck_word):
     """
     finds position of ith 1 and jth 0
     """
@@ -47,7 +47,7 @@ def findPos(i, j, dyckWord):
     counter0 = 0 # keep track of how many 0's have been found
     pos1 = -1
     pos0 = -1
-    for index, letter in enumerate(dyckWord):
+    for index, letter in enumerate(dyck_word):
         if letter == 1:
             counter1 += 1
             if counter1 == i:
@@ -63,69 +63,69 @@ def findPos(i, j, dyckWord):
     return pos1, pos0
 
 
-def combinedMove(currWord, move_type, distribution):
-    length=len(currWord) # pick random i,j between 1 and length
+def combined_move(curr_word, move_type, distribution):
+    length = len(curr_word) # pick random i,j between 1 and length
 
     if move_type == 'by_count':
-        i = random.randrange(1,length/2 + 1)
-        j = random.randrange(1,length/2 + 1)
-        pos1, pos0 = findPos(i, j, currWord)
+        i = random.randrange(1, length/2 + 1)
+        j = random.randrange(1, length/2 + 1)
+        pos1, pos0 = findPos(i, j, curr_word)
     elif move_type == 'by_position':
         pos1 = random.randrange(0, length)
         pos0 = random.randrange(0, length)
-        if currWord[pos1] == currWord[pos0]: # self-loop
+        if curr_word[pos1] == curr_word[pos0]: # self-loop
             return
 
     if distribution == 'nntm':
-        currWordEnergy = fasterEnergyFunction(currWord) # calculates energy before modifying currWord
+        curr_energy = calculate_energy(curr_word) # calculates energy before modifying curr_word
 
-    currWord[pos1], currWord[pos0] = currWord[pos0], currWord[pos1] # swap values; currWord is now newWord
+    curr_word[pos1], curr_word[pos0] = curr_word[pos0], curr_word[pos1] # swap values; curr_word is now new_word
 
-    if not isValid(currWord, end=max(pos1, pos0)):
-        currWord[pos1], currWord[pos0] = currWord[pos0], currWord[pos1] # no move to make; swap back
+    if not is_valid(curr_word, end=max(pos1, pos0)):
+        curr_word[pos1], curr_word[pos0] = curr_word[pos0], curr_word[pos1] # no move to make; swap back
         return
 
     if distribution == 'nntm': # if distribution is uniform, we make the above move 100% of the time (never swap back)
-        newWordEnergy = fasterEnergyFunction(currWord) # calculates energy
-        probability= min(1, np.exp(-newWordEnergy)/np.exp(-currWordEnergy)) # MCMC Part!!!!
-        ran=random.random()
-        if ran <= probability:  # leave new dyckword as current state
+        new_energy = calculate_energy(curr_word) # calculates energy
+        probability = min(1, np.exp(-new_energy)/np.exp(-curr_energy)) # MCMC part
+        ran = random.random()
+        if ran <= probability:  # leave new dyck_word as current state
             return
-        else: # revert to old dyckWord
-            currWord[pos1], currWord[pos0] = currWord[pos0], currWord[pos1] # swap back
+        else: # revert to old dyck_word
+            curr_word[pos1], curr_word[pos0] = curr_word[pos0], curr_word[pos1] # swap back
 
 
-def myProject(startWord, mixingTimeT, sampleInterval, numOfSamples, move_type, distribution):
+def my_project(start_word, mixing_time, sample_interval, num_samples, move_type, distribution):
     """
-    startWord: word to start with
-    mixingTimeT: t
-    sampleInterval: collect every x-amount of steps
-    numOfSamples: number of samples that I want
-    move_type: one of 'by_count' or 'by_position', passed to combinedMove
-    distribution: one of 'uniform' or 'nntm', passed to combinedMove
+    start_word: word to start with
+    mixing_time: t
+    sample_interval: collect every x-amount of steps
+    num_samples: number of samples that I want
+    move_type: one of 'by_count' or 'by_position', passed to combined_move
+    distribution: one of 'uniform' or 'nntm', passed to combined_move
     """
     assert move_type in ('by_count', 'by_position')
     assert distribution in ('uniform', 'nntm')
 
-    samples=[] # an empty list that will append the samples
-    currWord=startWord
-    for i in range(mixingTimeT):  # I need my movingWithProb to run mixingTimeT amount of times (while loop)
-        combinedMove(currWord, move_type, distribution)
-    sampCount=0
-    stepCount=0
-    while sampCount < numOfSamples:  # I need my program to stop after I have collected numOfSamples amount of samples
-        if stepCount==sampleInterval: # after sampleInterval amount of steps, append the currWord to my list 'samples' (for loop)
-            samples.append(list(currWord))
-            stepCount=0
-            sampCount+=1
+    samples = [] # an empty list that will append the samples
+    curr_word = start_word
+    for i in range(mixing_time):  # I need my movingWithProb to run mixing_time amount of times (while loop)
+        combined_move(curr_word, move_type, distribution)
+    samp_count = 0
+    step_count = 0
+    while samp_count < num_samples:  # I need my program to stop after I have collected num_samples amount of samples
+        if step_count == sample_interval: # after sample_interval amount of steps, append the curr_word to my list 'samples' (for loop)
+            samples.append(list(curr_word))
+            step_count = 0
+            samp_count += 1
         else:
-            combinedMove(currWord, move_type, distribution)
-            stepCount+=1
+            combined_move(curr_word, move_type, distribution)
+            step_count += 1
     return samples # return samples
 
 
 # energy function rewritten by Anna to be faster and avoid recursion depth issues
-def fasterEnergyFunction(word):
+def calculate_energy(word):
     root_deg = 0
     num_leaves = 0
     int_nodes = 0
@@ -156,7 +156,7 @@ def fasterEnergyFunction(word):
     return (-.4*root_deg +(2.3*num_leaves) +1.3*int_nodes) - .1*num_edges
 
 
-def contactDistances(word): # For any dyck word, returns the contact distances of the corresponding matching
+def contact_distances(word): # For any dyck word, returns the contact distances of the corresponding matching
     # formatted as a list l of length 2n, where l[i] is the number of pairs of cd i
     l = len(word)
     cds = [0 for i in range(l)]
@@ -167,7 +167,7 @@ def contactDistances(word): # For any dyck word, returns the contact distances o
         else: # end of arc
             begin = unpaired.pop()
             dist = pos-begin-1
-            cds[dist]+=1
+            cds[dist] += 1
     return cds
 
 
@@ -202,27 +202,27 @@ if __name__ == '__main__':
 
     start_time = time.time()
 
-    startWord = [1] * args.n + [0] * args.n
-    outPutSamples = myProject(startWord, args.mixing_time, args.sample_interval, args.num_samples, move_type, distribution)
+    start_word = [1] * args.n + [0] * args.n
+    output_samples = my_project(start_word, args.mixing_time, args.sample_interval, args.num_samples, move_type, distribution)
 
     end_time = time.time()
     print('Elapsed time was {:.0f} seconds.'.format(end_time - start_time))
 
     # compute contact distances
     cd_sums = [0 for i in range(2 * args.n)]
-    for sample in outPutSamples:
-        cds = contactDistances(sample)
+    for sample in output_samples:
+        cds = contact_distances(sample)
         cd_sums = map(add, cd_sums, cds)
 
     cd_sums = list(cd_sums)
 
-    cd_sums_name = 'sums_n={}_moveType={}_dist={}_mixingTime={}_sampleInterval={}_numOfSamples={}.txt'.format(args.n, move_type, distribution, args.mixing_time, args.sample_interval, args.num_samples)
+    cd_sums_name = 'sums_n={}_moveType={}_dist={}_mixingTime={}_sample_interval={}_num_samples={}.txt'.format(args.n, move_type, distribution, args.mixing_time, args.sample_interval, args.num_samples)
     cd_sums_name = os.path.join('data', cd_sums_name)
     print('saving cd_sums to: {}'.format(cd_sums_name))
 
     with open(cd_sums_name, 'w') as f:
         f.writelines('{}\n'.format(i) for i in cd_sums)
 
-    # ### for testing contactDistances
+    # ### for testing contact_distances
     # # test_word = DyckWord([1,1,1,0,1,0,0,1,0,0])
-    # # contactDistances(test_word)
+    # # contact_distances(test_word)
