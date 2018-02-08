1ff4c58b3368d2419d66e2abb5af09a0431687c8
Greg <greg9381@gmail.com>
2018-02-08 14:28:16 -0500

    adding new values in calculate_effective_sample_size

--- calculate_effective_sample_size.R
+++ calculate_effective_sample_size.R
@@ -3,7 +3,33 @@ library(coda)
 stats = c("num_leaves", "height", "root_degree")
 for (stat in stats)
 {
-	filename = sprintf("data/by_sample/%s_n=1000_dist=uniform_mixingTime=10000000_sampleInterval=1_numSamples=1000000.txt", stat)
+	n = 1000
+	mixingTime = 10000000
+
+	### first run
+	# sampleInterval = 1
+	# numSamples = 1000000
+
+	### second run based on first effective sample size
+	# sampleInterval = 354
+	# numSamples = 2820
+
+	### let's try that again
+	# sampleInterval = 1000
+	# numSamples = 1000
+
+	### let's try that again, with more accurate numbers
+	sampleInterval = 1075
+	numSamples = 930
+
+	filename = sprintf(
+		"data/by_sample/%s_n=%d_dist=uniform_mixingTime=%d_sampleInterval=%d_numSamples=%d.txt",
+		stat,
+		n,
+		mixingTime,
+		sampleInterval,
+		numSamples)
+
 	t = read.table(filename)
 	colnames(t)[1] <- stat
 	print(effectiveSize(t))
