aaa1d7ee75c21c99fb62fae40e9aa28f3850d5c0
Greg <greg9381@gmail.com>
2018-02-17 22:07:08 -0500

    using fewer samples, and saving plots to gelman.pdf

--- calculate_gelman_convergence.R
+++ calculate_gelman_convergence.R
@@ -1,16 +1,13 @@
 library(coda)
 
-m1 = as.mcmc(v1)
-m2 = as.mcmc(v2)
-
-mcmc_list = mcmc.list(m1, m2)
-
 stats = c("root_degree", "num_leaves", "height")
 
 n = 1000
 mixingTime = 0
-sampleInterval = 0
-numSamples = 30000000
+sampleInterval = 1
+numSamples = 10000000
+
+pdf('gelman.pdf')
 
 for (stat in stats) {
 	print(sprintf(
@@ -30,9 +27,10 @@ for (stat in stats) {
 			sampleInterval,
 			numSamples)
 
-		chains[[run]] = as.mcmc(scan(actual_values_filename))
+		chains[[run]] = as.mcmc(scan(actual_values_filename, nmax=1000000))
 	}
 
 	chains_list = do.call(mcmc.list, chains)
-	print(gelman.diag(chains_list))
+	print(gelman.diag(chains_list, transform=TRUE))
+	print(gelman.plot(chains_list, transform=TRUE))
 }
