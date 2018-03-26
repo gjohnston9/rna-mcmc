7e5262670b5b2b99540bab0b86b4ee71dc7c66cc
Greg <greg9381@gmail.com>
2018-03-26 17:37:54 -0400

    using common bin width for both datasets in create_histograms

--- create_histograms.py
+++ create_histograms.py
@@ -5,15 +5,24 @@ from argparse import Namespace
 import os
 
 def create_histogram(characteristic_name, uniform, nntm, save_name):
+	min_x = min(min(uniform), min(nntm))
+	max_x = max(max(uniform), max(nntm))
+	padding = (max_x - min_x) / 8.0 # adding an extra 1/8 of whitespace before and after data
+	min_x -= padding
+	max_x += padding
+	min_x = max(0, min_x)
+	bins = np.linspace(min_x, max_x, 76)
+
 	fig, ax = plt.subplots()
-	ax.hist(nntm, normed=True, color='red', alpha=0.5, label='thermodynamic distribution')
-	ax.hist(uniform, normed=True, color='green', alpha=0.5, label='uniform distribution')
+	ax.hist(nntm, bins=bins, color='red', alpha=0.5, label='thermodynamic distribution')
+	ax.hist(uniform, bins=bins, color='green', alpha=0.5, label='uniform distribution')
 
 	ax.set(
 		title='Comparison between {} under\nuniform and thermodynamic distributions'.format(characteristic_name),
 		xlabel=characteristic_name,
-		ylabel='frequency (normalized)')
+		ylabel='frequency')
 	ax.legend()
+	print('saving {} to {}'.format(characteristic_name, save_name))
 	plt.savefig(save_name)
 
 if __name__ == '__main__':
